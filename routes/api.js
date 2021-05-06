fd0e510b1a4cce9d00519763eef340c2c5332e4a
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-05-06 15:20:16 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -4081,10 +4081,44 @@ router.get('/news', async (req, res) => {
   if(!apikeyInput) return res.json(loghandler.notparam)
   if(apikeyInput !== `${key}`) return res.sendFile(invalidKey)
 
-	var data = fs.readFileSync(__path + '/lib/utils/news.js')
-	var json = JSON.parse(data)
-
-     res.json(json)
+     res.json({
+{
+    'jenis': 'cnn',
+    'type':["nasional","internasional","ekonomi","olahraga","teknologi","hiburan","gaya-hidup"],
+    'example': 'https://kuhong-api.herokuapp.com/api/news/cnn?type=teknologi&apikey=APIKEY',
+    'author': 'RC047'
+},
+{
+    'jenis': 'cnbc',
+    'type':["market","investment","news","entrepreneur","syariah","tech","lifestyle"],
+    'example': 'https://kuhong-api.herokuapp.com/api/news/cnbc?type=syariah&apikey=APIKEY',
+    'author': 'RC047'
+},
+{
+    'jenis': 'republika',
+    'type':["news","nusantara","khazanah","islam-digest","internasional","ekonomi","sepakbola","leisure"],
+    'example': 'https://kuhong-api.herokuapp.com/api/news/republika?type=ekonomi&apikey=APIKEY',
+    'author': 'RC047'
+},
+{
+    'jenis' : 'tempo',
+    'type':["nasional","bisnis","metro","dunia","bola","sport","cantik","tekno","otomotif","nusantara"],
+    'example': 'https://kuhong-api.herokuapp.com/api/news/tempo?type=bisnis&apikey=APIKEY',
+    'author': 'RC047'
+},
+{
+    'jenis': 'antara',
+    'type':["terkini","top-news","politik","hukum","ekonomi","metro","sepakbola","olahraga","humaniora","lifestyle","hiburan","dunia","infografik","tekno","otomotif","warta-bumi","rilis-pers"],
+    'example': 'https://kuhong-api.herokuapp.com/api/news/tempo?type=olahraga&apikey=APIKEY',
+    'author': 'RC047'
+},
+{
+    'jenis': 'kumparan',
+    'type': 'gak ada',
+    'example': 'https://kuhong-api.herokuapp.com/api/news/kumparan?&apikey=APIKEY',
+    'author': 'RC047'
+}
+     })
 })
 
 router.get('/ssweb', async (req, res, next) => {
@@ -5070,7 +5104,8 @@ router.get('/upload', async (req, res, next) => {
         if (!file) return res.json({ message: `Masukan parameter file_url` })
 
  try {
-       var media = await getBuffer(file)
+       var encmedia = await imageToBase64(file)
+       var media = Buffer.from(encmedia, 'base64')
        var result = await upload(media)
 
              res.json({
