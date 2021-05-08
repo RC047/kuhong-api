e670607885569082c02a1df62a953c352823760d
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-05-08 14:25:29 +0700

    Create upload_file.js

---
+++ routes/upload_file.js
@@ -0,0 +1,37 @@
+__path = process.cwd();
+
+var http = require('http');
+var fs = require('fs');
+var upload = require(__path + '/lib/upload.js');
+var formidable = require('formidable');
+var mv = require('mv');
+
+http.createServer(function (req, res) {
+
+    if (req.url === "/upload_file" && req.method === "GET"){
+      fs.readFileSync(__path + '/views/upload.html', (err, data) => {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        if (err) throw err;
+        res.end(data);
+      });
+    }
+
+    if (req.url == '/upload_file' && req.method === "POST") {
+      var form = new formidable.IncomingForm();
+
+      form.parse(req, function (err, fields, files) {
+        var inputPath = files.file_upload.path;
+        var outputPath = __path + '/tmp/' + files.file_upload.name;
+
+        mv(inputPath, outputPath, function (err) {
+          if (err) { throw err; }
+          console.log('File uploaded successfully');
+          var upload_files = upload(fs.readFileSync(outputPath))
+          return res.json({ result: upload_files })
+        });
+      });
+    } 
+
+  }).listen(8000);
+
+  console.log("server listening...");
