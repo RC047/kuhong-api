7a6fdf2f480208958f7acf46debd4b5dacb671f1
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-05-11 09:42:52 +0700

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -17,10 +17,6 @@ router.get('/docs', async (req, res) => {
     res.sendFile(__path + '/views/index.html')
 })
 
-router.get('/api/upload_image', async (req, res) => {
-    res.sendFile(__path + '/views/upload_image.html')
-})
-
 router.get('/api/game/pingpong', async  (req, res) => {
     res.sendFile(__path + '/views/pingpong.html')
 })
