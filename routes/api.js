2ff34618273b828c18bc15ba1b95c7f9da09557e
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-05-12 15:22:55 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -4080,7 +4080,7 @@ try {
     if(!apikeyInput) return res.json(loghandler.notparam)
     if (!(apikeyInput == `${free_apikey}` || apikeyInput == `${apikey}` || apikeyInput == `${custom_apikey}`)) return res.sendFile(invalidKey)
 
-      res.sendFile(__path + '/src/lucu/' + Math.floor(Math.random() * 47) + '.mp4')
+      res.sendFile(__path + '/src/lucu/' + pickRandom(['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','34','35','36','37','38','39','40','41','42','43','44','45','46']) + '.mp4')
 
 } catch (e) {
      console.log(e)
