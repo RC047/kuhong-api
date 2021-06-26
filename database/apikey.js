a00e41e52f7a4fe5aecb57586f9aa13554aaee5a
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-06-26 11:20:19 +0700

    Update apikey.js

--- database/apikey.js
+++ database/apikey.js
@@ -1,3 +1,10 @@
+__path = process.cwd();
+var { generateApikey } = require(__path + '/lib/generator.js');
+
+function getFreeApikey() {
+  return generateApikey()
+}
+
 function getApikey() {
   return 'QyiH67N1mWvbbJ891lpL67m_uy1oPHSlL01Vv-1qRi'
 }
