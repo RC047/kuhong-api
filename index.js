62e5ad41496d3871a0d97a8fe96ed0b1a20a37fc
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-06-26 12:23:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ app.use(cors());
 app.use(secure);
 app.use(express.static('public'));
 
-app.use('/', mainrouter)
-app.use('/api', apirouter)
+app.use('/', mainrouter);
+app.use('/api', apirouter);
 
 app.listen(PORT, () => {
-    console.log(color('Server running on port ' + PORT, 'green'))
-})
+    console.log(color('Server running on port ' + PORT, 'green'));
+});
 
 module.exports = app
