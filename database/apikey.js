1755ffc144801d276236640f4cc7c368d729e0cf
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-06-26 11:15:39 +0700

    Create apikey.js

---
+++ database/apikey.js
@@ -0,0 +1,9 @@
+function getApikey() {
+  return 'QyiH67N1mWvbbJ891lpL67m_uy1oPHSlL01Vv-1qRi'
+}
+
+function getCustomApikey() {
+  return '04102006'
+}
+
+module.exports = { getApikey, getCustomApikey }
