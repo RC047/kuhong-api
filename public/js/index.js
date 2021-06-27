170233df7e4318cf240b700ffd164dd26989222e
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-06-27 17:03:53 +0700

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -23,7 +23,7 @@ Silahkan masukan namamu untuk identitas diwebsite ini :)
 `.trim(), 'Guest');
 console.log(`LOGIN :\n${name} just logged in to your website`);
 
-window.setTimeout("setTimes();", 1000);
+window.setTimeout('setTimes();', 1000);
 function setTimes() {
     var date = new Date();
     var time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
@@ -32,9 +32,9 @@ function setTimes() {
     if (date.getHours() == 10 || date.getHours() == 11 || date.getHours() == 12 || date.getHours() == 13 || date.getHours() == 14) ucapan = 'Selamat Siang';
     if (date.getHours() == 15 || date.getHours() == 16 || date.getHours() == 17) ucapan = 'Selamat Sore';
     if (date.getHours() == 18 || date.getHours() == 19 || date.getHours() == 20 || date.getHours() == 21 || date.getHours() == 22 || date.getHours() == 23) ucapan = 'Selamat Malam';
-          setTimeout("setTimes();", 1000);
-          document.getElementById("time").innerHTML = time;
-          document.getElementById("notif").innerHTML = ucapan + '\n' + name + '!';
+          setTimeout('setTimes();', 1000);
+          document.getElementById('time').innerHTML = time;
+          document.getElementById('notif').innerHTML = ucapan + '\n' + name + '!';
 }
 
 var ads = new Date();
