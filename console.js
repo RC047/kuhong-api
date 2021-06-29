6d5e36d101182cf97e4c58d72adadafe8f662c8c
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-06-29 21:12:22 +0700

    More Info :)

--- console.js
+++ console.js
@@ -1,6 +1,7 @@
-/** Console Runner
+/** Console Runner :
 
-* Menjalankan Console JavaScript
-* Url: https://kuhong-api.herokuapp.com/api/run?console=console.log('examples');
-* Method: GET
-*/
+ * Menjalankan Console JavaScript
+ * Url: https://kuhong-api.herokuapp.com/api/run?console=console.log('examples');
+ * Parameter: console (text|string)
+ * Method: GET
+ */
