e6abeee2ce1810bc8133b804bb34157cf7dd6506
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-06-29 12:55:55 +0700

    Move Battery Function

--- public/js/index.js
+++ public/js/index.js
@@ -49,59 +49,7 @@ function setTimes() {
           document.getElementById('notif').innerHTML = notif;
 
     }
+
 xhr.open('GET', url, true);
 xhr.send();
 }
-
-var Battery = (function(self) {
-
-  var _events = 'chargingchange chargingtimechange dischargingtimechange levelchange'
-    , _battery = navigator.getBattery || navigator.battery || navigator.mozBattery
-    , _status = null
-    , _statusCallback = function() {}
-    , _updateCallback = function() {}
-    , STATUS_UNSUPPORTED = 'Not supported';
-
-  self.getStatus = function(fn) {
-    if(_status === STATUS_UNSUPPORTED) {
-      fn(null, _status);
-    } else if(_status) {
-      fn(_status);
-    } else {
-      _statusCallback = fn;
-    }
-  };
-
-  self.onUpdate = function(fn) {
-    _updateCallback = fn;
-  };
-
-  function eventHandler(status) {
-    _status = status;
-    _updateCallback(_status);
-  }
-
-  function registerEventHandler(battery) {
-    _events.split(' ').forEach(function(evt) {
-      battery.addEventListener(evt, eventHandler);
-    });
-  }
-
-  if(_battery instanceof Function) {
-    _battery.call(navigator)
-      .then(function(status) {
-        _status = status;
-        _statusCallback(_status);
-        registerEventHandler(_status);
-      }, function() {
-        _status = STATUS_UNSUPPORTED;
-      });
-  } else if(_battery) {
-    _status = _battery;
-    registerEventHandler(_battery);
-  } else {
-    _status = STATUS_UNSUPPORTED;
-  }
-
-  return self;
-})(Battery || {});
