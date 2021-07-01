23063d6a4c66510b08bfb088cc1316b0a07fd7f3
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-07-01 10:02:47 +0700

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -50,72 +50,6 @@ function setTimes() {
           document.getElementById('notif').innerHTML = notif;
 }
 
-// Battery Functions (to get a Battery Status)
-var battery = (function(self) {
-
-  var _events = 'chargingchange chargingtimechange dischargingtimechange levelchange'
-    , _battery = navigator.getBattery || navigator.battery || navigator.mozBattery
-    , _status = null
-    , _statusCallback = function() {}
-    , _updateCallback = function() {}
-    , STATUS_UNSUPPORTED = 'not supported';
-
-  /** Function: getStatus
-   * Register callback function to retrieve status
-   *
-   * Parameters:
-   * (Function) fn(status, error) - callback function
-   */
-  self.getStatus = function(fn) {
-    if(_status === STATUS_UNSUPPORTED) {
-      fn(null, _status);
-    } else if(_status) {
-      fn(_status);
-    } else {
-      _statusCallback = fn;
-    }
-  };
-
-  /* Function: onUpdate
-   * Register callback function when battery status changes
-   *
-   * Parameters:
-   * (Function) fn(status) - callback function
-   */
-  self.onUpdate = function(fn) {
-    _updateCallback = fn;
-  };
-
-  function eventHandler(status) {
-    _status = status;
-    _updateCallback(_status);
-  }
-
-  function registerEventHandler(battery) {
-    _events.split(' ').forEach(function(evt) {
-      battery.addEventListener(evt, eventHandler);
-    });
-  }
-
-  if(_battery instanceof Function) {
-    _battery.call(navigator)
-      .then(function(status) {
-        _status = status;
-        _statusCallback(_status);
-        registerEventHandler(_status);
-      }, function() {
-        _status = STATUS_UNSUPPORTED;
-      });
-  } else if(_battery) {
-    _status = _battery;
-    registerEventHandler(_battery);
-  } else {
-    _status = STATUS_UNSUPPORTED;
-  }
-
-  return self;
-})(battery || {});
-
 // Other Functions (more functions?)
 function runConsole() {
 
@@ -320,7 +254,7 @@ Remove Ads = 30K / Tahun
 All Packs = 75K / Bulan
 All Packs = 150K / Tahun
 
-*Silahkan pilih "Oke" untuk menghubungi Owner dan lanjut membeli.
+*Silahkan pilih "Oke" untuk lanjut membeli.
 `.trim());
 
    if (buy) {
