842f85e5af70e7ec7fddac37de7f208f5fa88036
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-07-02 18:50:22 +0700

    Delete console.js

--- console.js
+++
@@ -1,7 +0,0 @@
-/** Console Runner :
-
- * Menjalankan Console JavaScript
- * Url: https://kuhong-api.herokuapp.com/api/run?console=console.log('examples');
- * Parameter: {text|string}
- * Method: GET
- */
