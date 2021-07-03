a08dd97e636915ee26c0dd38b80deeb542e59b7c
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-07-03 15:33:24 +0700

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,3 @@
-// Loading will be start before the documentation pages
-window.location = 'https://kuhong-api.herokuapp.com/api';
-
-
 // Bar Functions (to set all bars)
 $(document).ready(function() {
 
