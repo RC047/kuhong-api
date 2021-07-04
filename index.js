3b0f26f4499099c53c965096ec23ebaeb46bea37
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-07-04 19:33:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express'),
     cors = require('cors'),
-    secure = require('ssl-express-www');
+    secure = require('ssl-express-www'),
+    fs = require('fs');
 
-const PORT = process.env.PORT || 8080 || 5000 || 3000
-var { color } = require('./lib/color.js')
+const port = process.env.PORT || 8080 || 5000 || 3000
+var { encryptHtml } = require('./lib/functions.js');
+var { color } = require('./lib/color.js');
 
 var mainrouter = require('./routes/main.js'),
     apirouter = require('./routes/api.js');
@@ -18,12 +20,13 @@ app.use(express.static('public'));
 app.use('/', mainrouter);
 app.use('/api', apirouter);
 app.use((req, res, next) => {
+   var notfound = fs.readFileSync(__path + '/views/404.html').toString();
    res.status(404);
-   res.sendFile(__path + '/views/404.html');
+   res.send(encryptHtml(notfound));
 });
 
-app.listen(PORT, () => {
-    console.log(color('Server running on port ' + PORT, 'green'));
+app.listen(port, () => {
+    console.log(color('Server running on port ' + port, 'green'));
 });
 
 module.exports = app
