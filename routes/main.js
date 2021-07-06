9aa2eb33059d08aa3a5291107c35bcfdac3958c1
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-07-06 19:22:57 +0700

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -14,7 +14,7 @@ var visits = await (await fetch('https://api.countapi.xyz/hit/kuhong-api.herokua
 var ip = req.ip || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
     if (blocked.indexOf(ip) > -1) return res.json({ message: 'Kamu telah diblokir oleh Owner!' })
     var home = await fs.readFileSync(__path + '/views/home.html').toString()
-    res.send(await encryptHtml(home))
+    res.send(home)
 })
 
 router.get('/api', async (req, res, next) => {
@@ -30,7 +30,7 @@ var visits = await (await fetch('https://api.countapi.xyz/hit/kuhong-api.herokua
 var ip = req.ip || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
     if (blocked.indexOf(ip) > -1) return res.json({ message: 'Kamu telah diblokir oleh Owner!' })
     var docs = await fs.readFileSync(__path + '/views/docs.html').toString()
-    res.send(docs)
+    res.send(await encryptHtml(docs))
 })
 
 router.get('/page/offline', async (req, res, next) => {
