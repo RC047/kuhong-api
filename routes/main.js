bab27556054db4a690c2bffca7b031c309ac281c
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-07-10 13:43:40 +0700

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -115,6 +115,17 @@ var ip = req.ip || req.connection.remoteAddress || req.socket.remoteAddress || r
     res.send(await encryptHtml(dino))
 })
 
+router.get('/botchat', async (req, res, next) => {
+var visits = await (await fetch('https://api.countapi.xyz/hit/kuhong-api.herokuapp.com/visits')).json()
+var ip = req.ip || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
+    if (blocked.indexOf(ip) > -1) return res.json({ message: 'Kamu telah diblokir oleh Owner!' })
+    var devMode = req.query.dev;
+    var bot = await fs.readFileSync(__path + '/views/botchat.html').toString()
+    var devTools = await fs.readFileSync(__path + '/views/tools.html').toString()
+    if (devMode == 'true') bot = bot + devTools
+    res.send(await encryptHtml(bot))
+})
+
 router.get('/js/botchat.js', async (req, res, next) => {
 var visits = await (await fetch('https://api.countapi.xyz/hit/kuhong-api.herokuapp.com/visits')).json()
 var ip = req.ip || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
