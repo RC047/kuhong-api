46606c400a95f2e86a29e26b29b0f355029db969
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-07-15 18:40:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ var typeJS = fs.readFileSync('./public/js/type.js').toString();
 var adsJS = fs.readFileSync('./public/js/ads.js').toString();
 var autoloadJS = fs.readFileSync('./public/js/autoload.js').toString();
 
-fs.writeFileSync('./public/js/botchat-v2.js', botchatJS.split('// (Change display if visitor not used Phone or Android)')[1]));
+fs.writeFileSync('./public/js/botchat-v2.js', botchatJS.split('// (Change display if visitor not used Phone or Android)')[1]);
 confuse.obfuscate(typeJS, { target: 'browser', preset: 'high', minify: true, stringEncoding: true }).then(data => fs.writeFileSync('./public/js/type.js', data));
 confuse.obfuscate(adsJS, { target: 'browser', preset: 'high', minify: true, stringEncoding: true }).then(data => fs.writeFileSync('./public/js/ads.js', data));
 confuse.obfuscate(autoloadJS, { target: 'browser', preset: 'high', minify: true, stringEncoding: true }).then(data => fs.writeFileSync('./public/js/autoload.js', data));
 
-var port = process.env.PORT || 8080 || 5000 || 3000;
+var PORT = process.env.PORT || 8000 || 5000 || 3000;
 var mainrouter = require('./routes/main.js'),
     apirouter = require('./routes/api.js');
 
@@ -35,8 +35,6 @@ app.use(async (req, res, next) => {
    res.send(await encryptHtml(notfound));
 });
 
-app.listen(port, () => {
-    console.log(color('Server running on port ' + port, 'green'));
-});
+app.listen(PORT, () => console.log(color('Server running on port ' + port, 'green')));
 
 module.exports = app
