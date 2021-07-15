7908268fc5cddd3ac3a0f9dd6ff348c0f7db87af
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-07-15 12:41:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,23 +6,17 @@ var confuse = require('js-confuser');
 var { encryptHtml } = require('./lib/functions.js');
 var { color } = require('./lib/color.js');
 
-var scriptJS = fs.readFileSync('./public/js/script.js').toString();
 var typeJS = fs.readFileSync('./public/js/type.js').toString();
 var botchatJS = fs.readFileSync('./public/js/botchat.js').toString();
 var adsJS = fs.readFileSync('./public/js/ads.js').toString();
 var autoloadJS = fs.readFileSync('./public/js/autoload.js').toString();
 
-confuse.obfuscate(scriptJS, { target: 'browser', preset: 'high', minify: true, renameVariables: false, renameGlobals: false, stringEncoding: false })
-.then(data => fs.writeFileSync('./public/js/script.js', data));
-confuse.obfuscate(typeJS, { target: 'browser', preset: 'high', minify: true, renameVariables: false, renameGlobals: false, stringEncoding: true })
+fs.writeFileSync('./public/js/botchat-v2.js', botchatJS);
+confuse.obfuscate(typeJS, { target: 'browser', preset: 'high', minify: true, stringEncoding: true })
 .then(data => fs.writeFileSync('./public/js/type.js', data));
-confuse.obfuscate(botchatJS.split('// Starting')[1], { target: 'browser', preset: 'medium', minify: true, renameVariables: false, renameGlobals: false, stringEncoding: false })
-.then(data => fs.writeFileSync('./public/js/botchat-v2.js', data));
-confuse.obfuscate(botchatJS, { target: 'browser', preset: 'high', minify: true, renameVariables: false, renameGlobals: false, stringEncoding: false })
-.then(data => fs.writeFileSync('./public/js/botchat.js', data));
-confuse.obfuscate(adsJS, { target: 'browser', preset: 'high', minify: true, renameVariables: false, renameGlobals: false, stringEncoding: true })
+confuse.obfuscate(adsJS, { target: 'browser', preset: 'high', minify: true, stringEncoding: true })
 .then(data => fs.writeFileSync('./public/js/ads.js', data));
-confuse.obfuscate(autoloadJS, { target: 'browser', preset: 'high', minify: true, renameVariables: false, renameGlobals: false, stringEncoding: true })
+confuse.obfuscate(autoloadJS, { target: 'browser', preset: 'high', minify: true, stringEncoding: true })
 .then(data => fs.writeFileSync('./public/js/autoload.js', data));
 
 var port = process.env.PORT || 8080 || 5000 || 3000;
