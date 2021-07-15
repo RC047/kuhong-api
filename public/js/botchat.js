6ef5c285e55a200b54701a2ab80f53a79bb7e001
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-07-15 17:46:30 +0700

    Update botchat.js

--- public/js/botchat.js
+++ public/js/botchat.js
@@ -6,11 +6,8 @@
  */
 
 
-// Mobile Mode :
-if (/Mobile|Android|Phone/i.test(navigator.userAgent) && window.location == 'https://kuhong-api.herokuapp.com/botchat?v2=true') window.location = 'https://kuhong-api.herokuapp.com/botchat';
-// PC Mode :
+// Display PC Mode :
 if (!(/Mobile|Android|Phone/i.test(navigator.userAgent) && window.location == 'https://kuhong-api.herokuapp.com/botchat')) window.location = 'https://kuhong-api.herokuapp.com/botchat?v2=true';
-
 var prefix = new RegExp('^[!?#/$.]');
 var baseCmd = pickRandom(['!', '?', '#', '/', '$', '.']) + pickRandom(['menu', 'help']);
 var previousMessage = '';
