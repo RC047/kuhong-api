b983ba3f00924eb1d494239b5f562beaa22d3df4
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-07-16 10:03:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -36,6 +36,6 @@ app.use(async (req, res, next) => {
    res.send(await encryptHtml(notfound));
 });
 
-app.listen(PORT, () => console.log(color('Server running on port', PORT, 'green')));
+app.listen(PORT, () => console.log(color('Server running on port ' + PORT, 'green')));
 
 module.exports = app
