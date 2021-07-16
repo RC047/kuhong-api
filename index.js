76dab98ef318f68e1ccfb51da87da5eb4dc98fda
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-07-16 10:07:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ var typeJS = fs.readFileSync('./public/js/type.js').toString();
 var adsJS = fs.readFileSync('./public/js/ads.js').toString();
 var autoloadJS = fs.readFileSync('./public/js/autoload.js').toString();
 
-confuse.obfuscate(deviceJS.split('// Mobile Only')[1], { target: 'browser', preset: 'high', minify: true, stringEncoding: true }).then(data => fs.writeFileSync('./public/js/mobile.js', data));
-confuse.obfuscate(deviceJS.split('// Mobile Only')[0], { target: 'browser', preset: 'high', minify: true, stringEncoding: true }).then(data => fs.writeFileSync('./public/js/window.js', data));
+confuse.obfuscate(deviceJS.split('// Mobile Only')[1], { target: 'browser', preset: 'high', minify: true, stringEncoding: true }).then(data => fs.writeFileSync('./public/js/window.js', data));
+confuse.obfuscate(deviceJS.split('// Mobile Only')[0], { target: 'browser', preset: 'high', minify: true, stringEncoding: true }).then(data => fs.writeFileSync('./public/js/mobile.js', data));
 confuse.obfuscate(typeJS, { target: 'browser', preset: 'high', minify: true, stringEncoding: true }).then(data => fs.writeFileSync('./public/js/type.js', data));
 confuse.obfuscate(adsJS, { target: 'browser', preset: 'high', minify: true, stringEncoding: true }).then(data => fs.writeFileSync('./public/js/ads.js', data));
 confuse.obfuscate(autoloadJS, { target: 'browser', preset: 'high', minify: true, stringEncoding: true }).then(data => fs.writeFileSync('./public/js/autoload.js', data));
