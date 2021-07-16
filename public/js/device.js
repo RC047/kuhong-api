c877acb63323c1f4aee40e5826a117221141411c
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-07-16 09:18:04 +0700

    Create device.js

---
+++ public/js/device.js
@@ -0,0 +1,5 @@
+// PC Only
+if (!/Mobile|Android|Phone/.test(navigator.userAgent)) window.location = 'https://kuhong-api.herokuapp.com/botchat?v2=true';
+
+// Mobile Only
+if (/Mobile|Android|Phone/.test(navigator.userAgent)) window.location = 'https://kuhong-api.herokuapp.com/botchat';
