627015c4991239e81ba076cdec90f1169c897c71
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-07-18 17:53:33 +0700

    Update platform.js

--- public/js/platform.js
+++ public/js/platform.js
@@ -5,17 +5,15 @@ if (!/Mobile|Android|Phone/.test(navigator.userAgent)) {
     var value = '';
     for (var i of url) value += i[0];
     if (window.location.toString().includes('?')) params = '&platform=window';
-    if (value == 'platformplatform') window.location = 'https://kuhong-api.herokuapp.com/botchat';
     window.location += params;
 }
 
 // Mobile Only
-if (/Mobile|Android|Phone/.test(navigator.userAgent)) {
+if (!/Window|Mac|PC/.test(navigator.userAgent)) {
     var url = new URL(window.location).searchParams.entries();
     var params = '?platform=mobile';
     var value = '';
     for (var i of url) value += i[0];
     if (window.location.toString().includes('?')) params = '&platform=mobile';
-    if (value == 'platformplatform') window.location = 'https://kuhong-api.herokuapp.com/botchat';
-    window.location += params
+    window.location += params;
 }
