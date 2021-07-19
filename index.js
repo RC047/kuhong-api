588ed334c52ef8da833049aeb9b6c44f20485970
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-07-19 10:18:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,14 +8,12 @@ var { color } = require('./lib/color.js');
 
 var scriptJS = fs.readFileSync('./public/js/script.js').toString();
 var botchatJS = fs.readFileSync('./public/js/botchat.js').toString();
-var platformJS = fs.readFileSync('./public/js/platform.js').toString();
 var typeJS = fs.readFileSync('./public/js/type.js').toString();
 var adsJS = fs.readFileSync('./public/js/ads.js').toString();
 var autoloadJS = fs.readFileSync('./public/js/autoload.js').toString();
 
 confuse.obfuscate(scriptJS, { target: 'browser', minify: true }).then(data => fs.writeFileSync('./public/js/script.js', data));
 confuse.obfuscate(botchatJS, { target: 'browser', minify: true }).then(data => fs.writeFileSync('./public/js/botchat.js', data));
-confuse.obfuscate(platformJS, { target: 'browser', preset: 'high', minify: true, stringEncoding: true }).then(data => fs.writeFileSync('./public/js/platform.js', data));
 confuse.obfuscate(typeJS, { target: 'browser', preset: 'high', minify: true, stringEncoding: true }).then(data => fs.writeFileSync('./public/js/type.js', data));
 confuse.obfuscate(adsJS, { target: 'browser', preset: 'high', minify: true, stringEncoding: true }).then(data => fs.writeFileSync('./public/js/ads.js', data));
 confuse.obfuscate(autoloadJS, { target: 'browser', preset: 'high', minify: true, stringEncoding: true }).then(data => fs.writeFileSync('./public/js/autoload.js', data));
