266c1757b8264c1d9796fcb5ce9985f85f38ecf3
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-07-19 19:02:55 +0700

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -144,7 +144,7 @@ var ip = req.ip || req.connection.remoteAddress || req.socket.remoteAddress || r
 
 router.post('/upload', async (req, res, next) => {
 var visits = await (await fetch('https://api.countapi.xyz/hit/kuhong-api.herokuapp.com/visits')).json()
-if (!req.body || req.method == 'GET') return res.json({ status: false, error: 'No files passed', info: 'use POST method with a buffer response to use this api' })
+if (!req.body) return res.json({ status: false, error: 'No files passed' })
 
 try {
 var buffer = req.body;
