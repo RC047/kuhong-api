d7a69fac4034cca779a679e302a43f25988ef1c0
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-07-20 13:44:46 +0700

    body parser

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var body = require('body-parser');
 var app = express();
 var cors = require('cors');
 var secure = require('ssl-express-www');
@@ -28,6 +29,7 @@ app.set('json spaces', 2);
 app.use(cors());
 app.use(secure);
 app.use(express.static('public'));
+app.use(body.urlencoded({ extended: false }));
 
 app.use('/', mainrouter);
 app.use('/api', apirouter);
