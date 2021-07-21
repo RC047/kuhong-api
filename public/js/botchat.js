f66c035bc4ac6f9adc80b332a86642afa0d5639c
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-07-21 10:17:53 +0700

    Update botchat.js

--- public/js/botchat.js
+++ public/js/botchat.js
@@ -194,7 +194,7 @@ var botName = isTag('KuhongBot (Verified): ', true);
 var media = '';
 if (/(?<=\S+)\.(png|jpg|jpeg|gif|webp)/gi.test(name)) media = unescape('%3Cimg%20height%3D%22100%22%20src%3D%22') + url + unescape('%22%3E%3C/img%3E');
 else if (/(?<=\S+)\.(mp4|mkv|avi|webm|mov)/gi.test(name)) media = unescape('%3Cvideo%20controls%20height%3D%22100%22%20src%3D%22') + url + unescape('%22%3E%3C/video%3E');
-else if (/(?<=\S+)\.(mp3|m4a|aac|wav|ogg)/gi.test(name)) media = unescape('%%3Caudio%20controls%20style%3D%22width%3A30px%3Bheight%3A75px%3B%22%20src%3D%22') + url + unescape('%22%3E%3C/audio%3E');
+else if (/(?<=\S+)\.(mp3|m4a|aac|wav|ogg)/gi.test(input.name)) media = unescape('%%3Caudio%20controls%20style%3D%22width%3A100px%3Bheight%3A25px%3B%22%20src%3D%22') + url + unescape('%22%3E%3C/audio%3E');
 else caption = false, media = unescape('%3Ca%20href%3D%22#download=') + name + unescape('%22%20onclick%3D%22downloadFile%28%27') + url + unescape('%27%29%3B%22%3E%3Ci%20class%3D%22fas%20fa-file%22%3E%3C/i%3E%20') + name + unescape('%3C/a%3E');
 if (caption) return document.getElementById("chat").innerHTML += newLine + getDate() + botName + newLine + media + newLine + caption;
 document.getElementById("chat").innerHTML += newLine + getDate() + botName + newLine + media;
