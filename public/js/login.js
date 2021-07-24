05f6d02f06f14b11e18fee3536ce5f610bfefa71
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-07-24 17:36:05 +0700

    Create login.js

---
+++ public/js/login.js
@@ -0,0 +1,25 @@
+function login() {
+
+var login = true;
+var name = null;
+
+while (login) {
+name = prompt(`
+LOGIN REQUIRED :
+
+*Silahkan masukan nama disini untuk melanjutkan :
+`.trim());
+
+if (name == null) alert('Login dibutuhkan!');
+else if (name == '') alert('Nama tidak bisa kosong!');
+else if (name !== '') login = false;
+}
+
+fetchURI('https://kuhong-api.herokuapp.com/api/login', {
+     method: 'POST',
+     headers: 'application/x-www-form-urlencoded',
+     body: 'name=' + name
+});
+
+if (login == false) return window.location = 'https://kuhong-api.herokuapp.com/docs';
+}
