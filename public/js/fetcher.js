988a74b185d4126dc381f524bbacb64e70af9647
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-07-25 18:22:48 +0700

    Update fetcher.js

--- public/js/fetcher.js
+++ public/js/fetcher.js
@@ -13,23 +13,23 @@ if (opts.responseType) xhr.responseType = opts.responseType;
 xhr.send(opts.body);
 var data = opts.responseType ? xhr.response : xhr.responseText;
 if (/json/i.test(xhr.getAllResponseHeaders())) data = JSON.parse(data);
-    return {
-	  status: xhr.status,
-	  statusText: xhr.statusText,
-	  headers:{
-	       	  requested: opts.headers ? opts.headers : 'default (text/html)',
-		  userAgent: navigator.userAgent,
-		  contentLength: xhr.getAllResponseHeaders().split('content-length: ')[1].split('content-type: ')[0],
-		  contentType: xhr.getAllResponseHeaders().split('content-type: ')[1].split(';')[0]
-	  },
-	  target: url,
-	  method: opts.method.toUpperCase(),
-	  body: opts.body,
-	  data: data
+      return {
+	    status: xhr.status,
+	    statusText: xhr.statusText,
+	    headers:{
+                    requested: opts.headers ? opts.headers : 'default (text/html)',
+		    userAgent: navigator.userAgent,
+		    contentLength: xhr.getAllResponseHeaders().split('content-length: ')[1].split('content-type: ')[0],
+		    contentType: xhr.getAllResponseHeaders().split('content-type: ')[1].split(';')[0]
+	    },
+	    target: url,
+	    method: opts.method.toUpperCase(),
+	    body: opts.body,
+	    data: data
+        }
       }
-    }
-   } catch (e) {
-    err(e);
+    } catch (e) {
+   return err(`Failed to ${opts.method.toUpperCase()} "${url}"`);
   }
 }
 
