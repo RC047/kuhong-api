29bd64a9e67aadce82b8198e8be51410a12b2327
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-07-25 15:53:34 +0700

    Update fetch.js

--- public/js/fetch.js
+++ public/js/fetch.js
@@ -1,33 +1,37 @@
 function fetchURI(url, opts = { method: 'GET', headers: false, responseType: false, body: null, username: false, password: false }) {
 
-if (!url) return err('url param cant be blank');
+if (!url) return err('param cant be blank');
 if (!/^http(s)?:\/\/(\w+:?\w*@)?(\S+)(:\d+)?((?<=\.)\w+)+(\/([\w#!:.?+=&%@!\-/])*)?/gi.test(url)) return err('Only absolute URLs are supported');
 
+try {
 if (url) {
 var xhr = new XMLHttpRequest();
-xhr.addEventListener('error', err);
-xhr.open(opts.method.toUpperCase(), url, false, opts.username, opts.password);
+if (opts.username && opts.password) xhr.open(opts.method.toUpperCase(), url, false, opts.username, opts.password);
+else xhr.open(opts.method.toUpperCase(), url, false);
 if (opts.headers) xhr.setRequestHeader('Content-type', opts.headers);
 if (opts.responseType) xhr.responseType = opts.responseType;
 xhr.send(opts.body);
 var data = opts.responseType ? xhr.response : xhr.responseText;
 if (/json/i.test(xhr.getAllResponseHeaders())) data = JSON.parse(data);
-
-  return {
-	status: xhr.status,
-	statusText: xhr.statusText,
-	headers:{
-		requested: opts.headers ? opts.headers : 'default (text/html)',
-		userAgent: navigator.userAgent,
-		contentLength: xhr.getAllResponseHeaders().split('content-length: ')[1].split('content-type: ')[0],
-		contentType: xhr.getAllResponseHeaders().split('content-type: ')[1].split(';')[0]
-	},
-	target: url,
-	method: opts.method.toUpperCase(),
-	body: opts.body,
-	data: data
+} catch (e) {
+    err(e);
+} finally {
+    return {
+	  status: xhr.status,
+	  statusText: xhr.statusText,
+	  headers:{
+	       	  requested: opts.headers ? opts.headers : 'default (text/html)',
+		  userAgent: navigator.userAgent,
+		  contentLength: xhr.getAllResponseHeaders().split('content-length: ')[1].split('content-type: ')[0],
+		  contentType: xhr.getAllResponseHeaders().split('content-type: ')[1].split(';')[0]
+	  },
+	  target: url,
+	  method: opts.method.toUpperCase(),
+	  body: opts.body,
+	  data: data
+      }
+    }
   }
- }
 }
 
 function err(logs) {
