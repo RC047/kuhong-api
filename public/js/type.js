cbda48e9496099ffe71ea4eabfee53ec6c22b696
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-07-25 19:29:40 +0700

    Update type.js

--- public/js/type.js
+++ public/js/type.js
@@ -1,4 +1,5 @@
-// Typing Functions
+var textArrayIndex = 0;
+var charIndex = 0;
 
 var typedTextSpan = document.querySelector(".typed-text"),
     textArray = ['WELCOME TO MY REST API ^_^', 'MOHON JANGAN DISPAM YAA', 'BIAR GAK DOWN,, XIXIXII'],
@@ -6,30 +7,28 @@ var typedTextSpan = document.querySelector(".typed-text"),
     erasingDelay = 100,
     newTextDelay = 2e3;
 
-var textArrayIndex = 0,
-    charIndex = 0;
-
 function type() {
-  charIndex < textArray[textArrayIndex].length ? (typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex), charIndex++, setTimeout(type, 200)) : setTimeout(erase, 2e3)
+  return charIndex < textArray[textArrayIndex].length ? (typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex), charIndex++, setTimeout(type, 200)) : setTimeout(erase, 2e3)
 }
 
 function erase() {
-  charIndex > 0 ? (typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1), charIndex--, setTimeout(erase, 100)) : (textArrayIndex++, textArrayIndex >= textArray.length && (textArrayIndex = 0), setTimeout(type, 1300))
+  return charIndex > 0 ? (typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1), charIndex--, setTimeout(erase, 100)) : (textArrayIndex++, textArrayIndex >= textArray.length && (textArrayIndex = 0), setTimeout(type, 1300))
 }
 
 document.addEventListener("DOMContentLoaded", (function() {
-setTimeout(type, 2250)
+  setTimeout(type, 2250)
 }));
 
 $(document).ready((function() {
-        $(".changeprofile").submit((function(e) {
-        var formObj, formURL = $(this).attr("action"),
-            formData = new FormData(this);
-        $.ajax({ url: formURL, type: "POST", data: formData, contentType: !1, cache: !1, processData: !1, beforeSend: function() {
-        $("button").attr("disabled", "disabled"),
-        $("input").attr("disabled", "disabled"),
-        $("a").attr("disabled", "disabled"),
-        $(".inibutton").html('<i class="fas fa-spinner fa-spin"></i> Wait')
+
+  $(".changeprofile").submit((function(e) {
+  var formObj, formURL = $(this).attr("action"),
+      formData = new FormData(this);
+      $.ajax({ url: formURL, type: "POST", data: formData, contentType: !1, cache: !1, processData: !1, beforeSend: function() {
+      $("button").attr("disabled", "disabled"),
+      $("input").attr("disabled", "disabled"),
+      $("a").attr("disabled", "disabled"),
+      $(".inibutton").html('<i class="fas fa-spinner fa-spin"></i> Wait')
         }, success: function(data) {
            data.includes("Success") ? ($("button").removeAttr("disabled", "disabled"),
            $("a").removeAttr("disabled", "disabled"),
@@ -39,15 +38,15 @@ $(document).ready((function() {
            },
            { type: "success", timer: 3e3, placement: { from: "top", align: "right" }
         }),
-        window.location = window.location) : ($("button").removeAttr("disabled", "disabled"),
-        $("a").removeAttr("disabled", "disabled"),
-        $("input").removeAttr("disabled", "disabled"),
-        $(".inibutton").html("Save"),
-        $.notify({ icon: "tim-icons icon-simple-remove", message: data },
-        { type: "danger", timer: 3e3, placement: { from: "top", align: "right" }
-        }))
-       }
-     }),
-     e.preventDefault()
-   }))
+      window.location = window.location) : ($("button").removeAttr("disabled", "disabled"),
+      $("a").removeAttr("disabled", "disabled"),
+      $("input").removeAttr("disabled", "disabled"),
+      $(".inibutton").html("Save"),
+      $.notify({ icon: "tim-icons icon-simple-remove", message: data },
+      { type: "danger", timer: 3e3, placement: { from: "top", align: "right" }
+      }))
+     }
+    }),
+   e.preventDefault()
+ }))
 }))
