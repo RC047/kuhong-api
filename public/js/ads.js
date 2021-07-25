8a2df6781494df0d632a17c8e6484b881d064e20
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-07-25 15:53:54 +0700

    Update ads.js

--- public/js/ads.js
+++ public/js/ads.js
@@ -48,7 +48,7 @@ var listAds = [
 
 var ads = pickRandom(listAds);
 var a = Math.floor(Math.random() * 10);
-var b = Math.floor(Math.random() * 10);
+var b = Math.floor(Math.random() * 7);
 if (a == b || b == a) {
     alert('ADS :\n\n' + ads.split(' url:')[0]);
     window.location = ads.split('url:')[1];
