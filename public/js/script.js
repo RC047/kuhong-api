4ba5d6a96ba5daf27bdc206d4e1fee6751436056
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-07-26 13:59:11 +0700

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -101,7 +101,7 @@ o Some Improvements!
 }
 
 function getApikey() {
-if (document.getELementById("login").innerHTML !== unescape('%3Ci%20class%3D%22fas%20fa-user%22%3E%3C/i%3EMy%20Account')) return alert('Login terlebih dahulu untuk mendapatkan apikey!');
+if (document.getElementById("login").innerHTML !== unescape('%3Ci%20class%3D%22fas%20fa-user%22%3E%3C/i%3EMy%20Account')) return alert('Login terlebih dahulu untuk mendapatkan apikey!');
 var res = fetchURI('https://kuhong-api.herokuapp.com/api/getapikey');
   return prompt('GET APIKEY :\n\nSilahkan salin apikeynya disini\n\n*' + res.data.info, res.data.free_apikey);
 }
