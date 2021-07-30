588f09e71fa85c89e2d93c81153aa0ca1b63e9aa
RendyCraft047 <75566525+RC047@users.noreply.github.com>
2021-07-30 10:34:05 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -653,9 +653,8 @@ try {
 function reply(message, message2, message3) {
 
 if (!message) message = ''
-else message = ...message
 console.log(`${isMessageFromGroup ? groupName : senderName}: ${senderMessage}`)
-console.log(`Kuhong: ${message.join('\nKuhong: ')}`)
+console.log(`Kuhong: ${message + '\n' + message2 + '\n + message3}`)
 fetch('https://api.countapi.xyz/hit/kuhong-api.herokuapp.com/botreply').then(res => res.text())
 var messages = [{ message: message }]
 if (message && message2) messages.push({ message: message2 })
